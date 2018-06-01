f0a3893e24e17b998c7e1a200477e37356a6c8f4
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 22:45:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def self.create_table
-    sql = <<- SQL
+    sql = <<-SQL
     CREATE TABLE students (
       id INTEGER PRIMARY KEY,
       name TEXT,
@@ -27,7 +27,7 @@ class Student
   end
 
   def save
-    sql = <<-SQL
+    sql = <<- SQL
     INSERT INTO students (name, grade)
     VALUES (?, ?)
     SQL
